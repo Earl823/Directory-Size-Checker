0a5cacc96ae915818a47a81519310f1b1a05c815
Earl Joseph Bayron <57792991+Earl823@users.noreply.github.com>
2023-09-08 23:58:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+#!/usr/bin/env python3               
 
+"""                     
+python directory size checker /path/to/directory
+Total size: 2.5 GB          
+
+"""                            
+
+import sys                        
+import os
+
+loc = sys.argv[1]
+files = os.listdir(loc)
+size = 0
+
+for x in files:
+    file_path = os.path.join(loc, x)
+    file_size = os.path.getsize(file_path)
+    size += file_size
+
+total_size = round(size / (1024*1024), 3)
+
+if __name__ == '__main__':
+    try:
+        print(f'Total size: {total_size} GB')
+
+    except FileNotFoundError:
+        print('error')
